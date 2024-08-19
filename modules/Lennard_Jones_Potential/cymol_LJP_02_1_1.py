e6d04d83b2e54b1cbb9e4463ab55962af7136b35
Luca Arrigo Zammataro <luca.zammataro@gmail.com>
2024-08-18 19:29:09 -0500

    Update cymol_LJP_02_1_1.py

--- modules/Lennard_Jones_Potential/cymol_LJP_02_1_1.py
+++ modules/Lennard_Jones_Potential/cymol_LJP_02_1_1.py
@@ -1,3 +1,10 @@
+'''
+Main Code: cymol_LJP_02_1_1
+Title: Cymol Lennard Jones Potential
+Author: Luca Zammataro, Copyright (c) 2024
+Reference: https://towardsdatascience.com/the-lennard-jones-potential-35b2bae9446c
+This project is licensed under the GNU General Public License v3.0
+'''
 import cymol_LJP_02_1_1
 import os
 
