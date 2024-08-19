40f6226301e1a5ba68ab40f869e3ce109050fc93
Luca Arrigo Zammataro <luca.zammataro@gmail.com>
2024-08-18 19:30:39 -0500

    Update setup.py

--- modules/Lennard_Jones_Potential/setup.py
+++ modules/Lennard_Jones_Potential/setup.py
@@ -1,4 +1,12 @@
-# python setup.py build_ext --inplace
+'''
+Module: cymol_LJP_02_1_1
+Title: Cymol Lennard Jones Potential
+Author: Luca Zammataro, Copyright (c) 2024
+Compile: python setup.py build_ext --inplace
+Reference: https://towardsdatascience.com/the-lennard-jones-potential-35b2bae9446c
+This project is licensed under the GNU General Public License v3.0
+'''
+
 from setuptools import setup
 from Cython.Build import cythonize
 import numpy as np
