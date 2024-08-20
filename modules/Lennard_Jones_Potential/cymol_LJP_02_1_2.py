74761e19c9ade067b6967f800b1941ade86c3ee8
Luca Arrigo Zammataro <luca.zammataro@gmail.com>
2024-08-20 16:06:50 -0500

    Add files via upload

---
+++ modules/Lennard_Jones_Potential/cymol_LJP_02_1_2.py
@@ -0,0 +1,19 @@
+import cymol_LJP_02_1_2
+import os
+
+def main():
+  
+    # Define simulation parameters
+    deltaT = 0.005
+    density = 0.5
+    initUcell_x = 20
+    initUcell_y = 20
+    stepAvg = 1
+    stepLimit = 10000
+    temperature = 3.0
+
+    # Run the simulation
+    cymol_LJP_02_1_2.run_simulation(deltaT, density, initUcell_x, initUcell_y, stepAvg, stepLimit, temperature)
+
+if __name__ == "__main__":
+    main()
