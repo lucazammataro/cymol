6553dbef104b5ee5cb18f3d48069b326921e6a3c
Luca Arrigo Zammataro <luca.zammataro@gmail.com>
2024-08-20 16:41:17 -0500

    Update setup_cymol_LJP_02_1_2.py

--- modules/Lennard_Jones_Potential/setup_cymol_LJP_02_1_2.py
+++ modules/Lennard_Jones_Potential/setup_cymol_LJP_02_1_2.py
@@ -1,8 +1,10 @@
-# Module: cymol_LJP_02_1_2
-# Title: Cymol Lennard Jones Potential 2D version
-# Author: Luca Zammataro, Copyright (c) 2024
-# Compile: python setup_cymol_LJP_02_1_2.py build_ext --inplace
-# Reference: https://towardsdatascience.com/the-lennard-jones-potential-35b2bae9446c
+'''
+Title: Cymol Lennard Jones Potential 2D version
+Author: Luca Zammataro, Copyright (c) 2024
+code: setup
+Compile: python setup_cymol_LJP_02_1_2.py build_ext --inplace
+Reference: https://towardsdatascience.com/the-lennard-jones-potential-35b2bae9446c
+'''
 
 from setuptools import setup
 from Cython.Build import cythonize
